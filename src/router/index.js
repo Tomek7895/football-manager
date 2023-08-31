3f134d8b3d61e4ba489a6d3130a34c6acc058aab
TL <TL@gmail.com>
2023-08-31 11:40:40 +0200

    Create routes for Views Components

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,36 @@ const routes = [
     name: "Dashboard",
     component: () => import('@/views/DashboardView.vue'),
   },
+  {
+    path: '/calendar',
+    name: "Calendar",
+    component: () => import('@/views/CalendarView.vue'),
+  },
+  {
+    path: '/news',
+    name: "News",
+    component: () => import('@/views/NewsView.vue'),
+  },
+  {
+    path: '/profile',
+    name: "Profile",
+    component: () => import('@/views/PlayerProfileView.vue'),
+  },
+  {
+    path: '/settings',
+    name: "Settings",
+    component: () => import('@/views/SettingsView.vue'),
+  },
+  {
+    path: '/table',
+    name: "Table",
+    component: () => import('@/views/TableView.vue'),
+  },
+  {
+    path: '/asistant',
+    name: "Asistant",
+    component: () => import('@/views/VirtualAsistantView.vue'),
+  },
 ]
 
 const router = createRouter({
