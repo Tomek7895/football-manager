99fff7c010a6702d6e850fa4f7f5f69e76a03232
TL <TL@gmail.com>
2023-08-31 11:53:47 +0200

    Add routes to Views components to MainNav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -14,9 +14,62 @@
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-        <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+            <router-link :to="{ name: 'Dashboard' }"
+            class="text-decoration-none">
+                <v-list-item
+                base-color="cyan-darken-3"
+                title="Dashboard"
+                value="dashboard">
+                </v-list-item>
+            </router-link>
+            <router-link :to="{ name: 'News' }"
+            class="text-decoration-none">
+                <v-list-item
+                base-color="grey-darken-4"
+                title="Aktualności"
+                value="aktualności">
+                </v-list-item>
+            </router-link>
+            <router-link :to="{ name: 'Profile' }"
+            class="text-decoration-none">
+                <v-list-item
+                base-color="grey-darken-4"
+                title="Profil Gracza"
+                value="profil">
+                </v-list-item>
+            </router-link>
+            <router-link :to="{ name: 'Calendar' }"
+            class="text-decoration-none">
+                <v-list-item
+                base-color="grey-darken-4"
+                title="Kalendarz"
+                value="kalendarz">
+                </v-list-item>
+            </router-link>
+            <router-link :to="{ name: 'Table' }"
+            class="text-decoration-none">
+                <v-list-item
+                base-color="grey-darken-4"
+                title="Tabela"
+                value="tabela">
+                </v-list-item>
+            </router-link>
+            <router-link :to="{ name: 'Settings' }"
+            class="text-decoration-none">
+                <v-list-item
+                base-color="grey-darken-4"
+                title="Ustawienia"
+                value="ustawienia">
+                </v-list-item>
+            </router-link>
+            <router-link :to="{ name: 'Asistant' }"
+            class="text-decoration-none">
+                <v-list-item
+                base-color="grey-darken-4"
+                title="Virtualny Asysten"
+                value="asystent">
+                </v-list-item>
+            </router-link>
         </v-list>
     </v-navigation-drawer>
   </template>
