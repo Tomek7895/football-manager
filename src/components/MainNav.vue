9621a370288362227e1aae66823623a4d35a44c0
TL <TL@gmail.com>
2023-08-31 12:04:06 +0200

    Add Icons to MainNav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -17,6 +17,7 @@
             <router-link :to="{ name: 'Dashboard' }"
             class="text-decoration-none">
                 <v-list-item
+                prepend-icon="mdi-view-dashboard-edit"
                 base-color="cyan-darken-3"
                 title="Dashboard"
                 value="dashboard">
@@ -25,6 +26,7 @@
             <router-link :to="{ name: 'News' }"
             class="text-decoration-none">
                 <v-list-item
+                prepend-icon="mdi-newspaper-variant"
                 base-color="grey-darken-4"
                 title="Aktualności"
                 value="aktualności">
@@ -33,6 +35,7 @@
             <router-link :to="{ name: 'Profile' }"
             class="text-decoration-none">
                 <v-list-item
+                prepend-icon="mdi-account-card"
                 base-color="grey-darken-4"
                 title="Profil Gracza"
                 value="profil">
@@ -41,6 +44,7 @@
             <router-link :to="{ name: 'Calendar' }"
             class="text-decoration-none">
                 <v-list-item
+                prepend-icon="mdi-calendar-clock-outline"
                 base-color="grey-darken-4"
                 title="Kalendarz"
                 value="kalendarz">
@@ -49,6 +53,7 @@
             <router-link :to="{ name: 'Table' }"
             class="text-decoration-none">
                 <v-list-item
+                prepend-icon="mdi-table"
                 base-color="grey-darken-4"
                 title="Tabela"
                 value="tabela">
@@ -57,6 +62,7 @@
             <router-link :to="{ name: 'Settings' }"
             class="text-decoration-none">
                 <v-list-item
+                prepend-icon="mdi-cog"
                 base-color="grey-darken-4"
                 title="Ustawienia"
                 value="ustawienia">
@@ -65,9 +71,15 @@
             <router-link :to="{ name: 'Asistant' }"
             class="text-decoration-none">
                 <v-list-item
+                prepend-icon="mdi-google-assistant"
                 base-color="grey-darken-4"
                 title="Virtualny Asysten"
                 value="asystent">
+                    <template v-slot:append>
+                        <v-badge color="green-darken-2"
+                        content="Beta"
+                        inline></v-badge>
+                    </template>
                 </v-list-item>
             </router-link>
         </v-list>
