3a28d3988607b1b31be812036296c3c059f7f3d2
TL <TL@gmail.com>
2023-08-31 12:27:31 +0200

    Edit TopMenu

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,14 +1,23 @@
 <template>
     <v-app-bar :elevation="3"
       density="compact">
-      <div class="d-flex mx-auto">
-        <p>Testy</p>
+      <v-spacer></v-spacer>
+      <div class="d-flex mr-4">
+        <router-link :to="{ name: 'Settings' }"
+        class="text-grey-darken-4">
+            <v-btn icon="mdi-cog">
+            </v-btn>
+        </router-link>
+        <router-link :to="{ name: 'Calendar' }"
+        class="text-grey-darken-4">
+            <v-btn icon="mdi-logout-variant">
+            </v-btn>
+        </router-link>
       </div>
     </v-app-bar>
   </template>
   
   <script>
-
   </script>
   
   <style>
