e7a5f4c6b536ae3bba7ad2a15b766b150ae8753a
TL <TL@gmail.com>
2023-08-31 12:36:01 +0200

    Create Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-bottom-navigation>
+      <v-footer class="d-flex flex-row justify-end">
+        <div>
+          <v-btn v-for="icon in icons"
+            :key="icon"
+            class="mx-1"
+            :icon="icon"
+            variant="text">
+          </v-btn>
+        </div>
+      </v-footer>
+    </v-bottom-navigation>
+  </template>
+  
+  <script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-instagram',
+      ],
+    }),
+  }
+  </script>
+  
+  <style>
+</style>
+  
--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -4,12 +4,15 @@
     <v-main>
         <router-view />
     </v-main>
+    <Footer />
 </template>
 
 <script>
 import MainNav from '@/components/MainNav.vue'
 import TopMenu from '@/components/TopMenu.vue'
+import Footer from '@/components/Footer.vue'
+
 export default {
-    components: { MainNav, TopMenu }
+    components: { MainNav, TopMenu, Footer }
 }
 </script>
