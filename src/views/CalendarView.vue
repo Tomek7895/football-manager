3b8db50418208862d705fd0cf65d51c9fe439e1c
TL <TL@gmail.com>
2023-08-31 11:34:50 +0200

    Create Views components

---
+++ src/views/CalendarView.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="d-flex justify-center mb-6 bg-cyan-darken-3">Calendar View</h1>
+  </template>
+  
+  <script>
+  </script>
+  
---
+++ src/views/PlayerProfileView.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="d-flex justify-center mb-6 bg-cyan-darken-3">Player Profile View</h1>
+  </template>
+  
+  <script>
+  </script>
+  
