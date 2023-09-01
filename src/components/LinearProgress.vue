5ad1b9e2c1e3820a3bc3ea7fb2ca97f6c9d9ef6b
TL <TL@gmail.com>
2023-09-02 00:04:03 +0200

    Edit Asistant Component

---
+++ src/components/LinearProgress.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-progress-linear :model-value="value"
+      height="10"
+      bg-color="rgb(105, 105, 101)"
+      color="rgba(19, 213, 213, 0.5)"
+      rounded-bar="true"
+      class="mt-4">
+    </v-progress-linear>
+    <p class="d-flex justify-center">{{ value }}%</p>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      value: {
+        type: Number,
+        required: true
+      }
+    },
+  }
+  </script>
+  
--- src/views/VirtualAsistantView.vue
+++ src/views/VirtualAsistantView.vue
@@ -1,7 +1,19 @@
 <template>
     <h1 class="d-flex justify-center mb-6 bg-cyan-darken-3">Virtual Asistant View</h1>
+    <LinearProgress :value="value" />
   </template>
   
   <script>
+import LinearProgress from '@/components/LinearProgress.vue'
+export default {
+  components: {
+    LinearProgress
+  },
+  data() {
+    return {
+      value: 50
+    }
+  },
+}
   </script>
   
