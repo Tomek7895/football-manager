47a130e2f5f6b818a4725853002974e2066b5e7b
TL <TL@gmail.com>
2023-09-12 14:47:06 +0200

    testy

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="d-flex justify-center mb-6 bg-cyan-darken-3">Dashboard View</h1>
+    <h1 class="d-flex justify-center mb-6 bg-cyan-darken-3">Dashboard1 View</h1>
   </template>
   
   <script>
